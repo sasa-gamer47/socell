d5f66b6f79895ff9b104d410d8567f3d785fdc16
sasa-gamer47 <giacomofilippi237@gmail.com>
2022-05-06 17:12:04 +0200

    cloudinary fixed, and publishing posts works

--- pages/[id]/new.jsx
+++ pages/[id]/new.jsx
@@ -20,33 +20,16 @@ const New = () => {
     console.log('my image: ',myImage);
     
     // const cloudinary = require('cloudinary').v2
-    const [url, setUrl] = useState('')
-    const [files, setFiles] = useState(null)
-    const img = useRef(null)
+    const [isSuccess, setIsSuccess] = useState(false)
     const content = useRef(null)
     const [image, setImage] = useState('')
     const router = useRouter()
-    const [success, setSuccess] = useState(true)
+    const [success, setSuccess] = useState(false)
+    const [error, setError] = useState(false)
     const [loading, setLoading] = useState(false)
     const { query: { id } } = router
     // console.log(router);
 
-    const UPLOADCARE_LOCALE = 'it'
-    const UPLOADCARE_LOCALE_TRANSLATIONS = {
-        buttons: {
-            choose: {
-            files: {
-                one: 'Scegli un file',
-            },
-            },
-        },
-    }
-
-    if (typeof window !== 'undefined') { 
-        var file = uploadcare.fileFrom('url', 'http://example.com/image.png')
-
-        console.log(file);
-    }
 
 
     return (
@@ -54,7 +37,7 @@ const New = () => {
                 
             <div className='absolute flex items-center justify-center dark:text-white bottom-14 sm:bottom-0 top-14 w-full'>
                 {/* <img src={url} ref={img} width='500' height='500' /> */}
-                <form method='POST' action='/api/post/' onSubmit={(e) => {
+                <form method='POST' action='/api/post/' onSubmit={async (e) => {
                     e.preventDefault()
                     // console.log(e);
 
@@ -65,204 +48,82 @@ const New = () => {
                     // console.log(formData);
                     if (!loading) {
 
-                        console.log({
-                            img: image,
-                            content: content.current.value,
-                            user: id,
-                            comments: [],
-                        })
-                        console.log('image: ', image);
-                        fetch('/api/post/', {
+                        const fileInput = Array.from(e.currentTarget.elements).find(({ name }) => name === 'file');
+
+                        const formData = new FormData()
+
+                        for (const file of fileInput.files) {
+                            formData.append('file', file)
+                        }
+
+                        formData.append('upload_preset', 'my-uploads')
+
+                        const data = await fetch('https://api.cloudinary.com/v1_1/dcrsevgpq/image/upload', {
                             method: 'POST',
-                            body: JSON.stringify({
-                                img: image,
-                                content: content.current.value,
-                                user: id,
-                                comments: [],
-                            }),
-                            headers: {
-                                'content-type': 'application/json'
-                            },
-                        })
-                        .then((res) => res.json())
-                        .then((res) => {
-                            console.log(res)
-    
-    
-                            if (res.success) {
+                            body: formData
+                        }).then(r => r.json()).then(data => {
+                            console.log('cldnr response', data)
+                            if (data) {
                                 setSuccess(true)
-    
-                                setTimeout(() => {
-                                    setSuccess(false)
-                                }, 1000)
-                                setTimeout(() => { 
-                                    router.push('/')
-                                }, 2000)
+
+                                fetch('/api/post/', {
+                                    method: 'POST',
+                                    body: JSON.stringify({
+                                        img: data.secure_url,
+                                        content: content.current.value,
+                                        user: id,
+                                        comments: [],
+                                    }),
+                                    headers: {
+                                        'content-type': 'application/json'
+                                    },
+                                })
+                                .then((res) => res.json())
+                                .then((res) => {
+                                    console.log(res)
+            
+            
+                                    if (res.success) {
+                                        setSuccess(true)
+            
+                                        setTimeout(() => {
+                                            setSuccess(false)
+                                        }, 1000)
+                                        setTimeout(() => { 
+                                            router.push('/')
+                                        }, 2000)
+                                    }
+                                })
                             }
+                            
                         })
+
+                            // setUploadData(data);
+                        
+
+                        // console.log({
+                        //     img: image,
+                        //     content: content.current.value,
+                        //     user: id,
+                        //     comments: [],
+                        // })
+                        console.log('image: ', image);
+                        
+                       
                     }
 
 
                 }} className='drop-shadow-lg w-10/12 sm:w-4/12 bg-zinc-200 dark:bg-zinc-700 rounded-lg flex flex-col items-center gap-y-2'>
                     <h1 className='text-3xl font-semibold mb-4 mt-2'>Crea un nuovo post</h1>
-                    <input
-                        type="hidden"
-                        role="uploadcare-uploader"
-                        data-public-key="50a290e85f3101ba9ed1"
-                        data-tabs="file camera url facebook gdrive gphotos"
-                        onChange={(e) => { 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                            if (typeof window !== 'undefined') { 
-                            var file = uploadcare.fileFrom('object', e.target.files[0]);
-
-                                // console.log(file);
-                                
-                            file.progress(function (uploadInfo) {
-                                // console.log('progress: ', uploadInfo);
-                                // State of your upload gets updated.
-                                // The callback is invoked at least once with a current state,
-                                // right after assignment.
-                            })
-
-                            file.done(function(fileInfo) {
-                            // Upload has successfully completed and a file is ready.
-                                // console.log('success');
-                                // console.log(fileInfo);
-                            }).fail(function (error, fileInfo) {
-                                // console.log('failed...');
-                                
-                                // console.log(fileInfo);
-                                // console.log(error);
-                            // Upload failed, or something else went wrong, a file is not ready.
-                            });
-                        }
-                        }}
-                    />
-                    <input onChange={async (e) => {
-                        console.log(e.target.files[0]);
-                        // console.log(JSON.parse(e.target.files[0]))
-
-                        const currFile = e.target.files[0]
-                        // const fd = new FormData()
-                        // fd.append('image', currFile)
-                        
-                        // console.log(fd.get('image'));
-
-
-                        // const uploadData = await fetch('/api/upload')
-
-                        // console.log('uploadData: ', uploadData);
-
-                        fetch('/api/upload/', {
-                            method: 'POST',
-                            body: JSON.stringify({
-                                name: currFile.name,
-                                size: currFile.size,
-                                type: currFile.type,
-                            }),
-                            headers: {
-                                'content-type': 'application/json',
-                            },
-                        }).then(res => res.json()).then(data => {
-                            console.log('data: ', data);
-                        })
-
-
-
-
-
-
-
-
-
-
-
-
-                        if (typeof window !== 'undefined') { 
-                            var file = uploadcare.fileFrom('object', e.target.files[0]);
-
-                            console.log(file);
-
-                            file.progress(function (uploadInfo) {
-                                console.log('progress: ', uploadInfo)
-                               // State of your upload gets updated.
-                               // The callback is invoked at least once with a current state,
-                               // right after assignment.
-                            })
-
-                            file.done(function(fileInfo) {
-                            // Upload has successfully completed and a file is ready.
-                                console.log('success');
-                                console.log(fileInfo);
-                            }).fail(function (error, fileInfo) {
-                                console.log('failed...');
-                                
-                                console.log(fileInfo);
-                                console.log(error);
-                            // Upload failed, or something else went wrong, a file is not ready.
-                            });
-                        }
-                            
-                        
-                        // const File = await cloudinary.upload(e.target.files[0])
-                        // console.log('cloudinary: ', File);
-                        cloudinary.config(
-                            'cloud_name', 'dzq0qjvxg'
-                        )
-
-                        cloudinary.init()
-                        const url = cloudinary.url('example.png')
-                        // console.log(cloudinary);
-                        // console.log(url);
-                        
-                        // const files = e.target.files
-                        // const data = new FormData()
-
-                        // data.append('file', files[0])
-                        // data.append('upload_preset', 'socell')
-                        // // data.append('api_key', '611918792535995')
-
-                        // // console.log(data.getAll('upload_preset'));
-
-
-
-                        // setLoading(true)
-
-
-                        // console.log(files);
-    
-                        // const res = await fetch('https://api.cloudinary.com/v1_1/dcrsevgpq/image/upload', {
-                        //     method: 'POST',
-                        //     // headers: {
-                        //     //     'Authorization': 'Basic ' + base64.encode(process.env.CLOUDINARY_URL),
-                        //     // },
-                        //     body: data,
-                        // })
-
-                        // const file = await res.json()
-                        // console.log(file);
-
-                        // console.log(file.secure_url)
-
-                        // setImage(file.secure_url)
-                        // setLoading(false)
-
-                    }} className='file:rounded-lg file:border-0 file:outline-0 file:bg-zinc-300 dark:file:bg-zinc-600 dark:file:text-white file:p-1 file:px-4 file:text-lg file:mr-5 file:cursor-pointer transition file:duration-300 file:hover:bg-zinc-400 dark:file:hover:bg-zinc-500 ' type="file" name="file" id="file" />
+                    <input className='file:rounded-lg file:border-0 file:outline-0 file:bg-zinc-300 dark:file:bg-zinc-600 dark:file:text-white file:p-1 file:px-4 file:text-lg file:mr-5 file:cursor-pointer transition file:duration-300 file:hover:bg-zinc-400 dark:file:hover:bg-zinc-500 ' type="file" name="file" id="file" />
                     <textarea ref={content} name="" id="" className='text-lg resize-y h-40 w-full mt-5 bg-transparent p-3 outline-none rounded-lg' placeholder='Scrivi qui ciò che pensi...'></textarea>
-                    <input className={`${success ? 'bg-green-500' : ''} mt-10 mb-5 text-lg font-semibold p-2 px-7 rounded-lg drop-shadow-lg cursor-pointer bg-zinc-300 dark:bg-zinc-600 transition duration-300 hover:bg-zinc-400 dark:hover:bg-zinc-500 hover:-translate-y-2 hover:scale-110`} type="submit" value="Crea post" />
+                    <input className={`${success ? 'bg-green-500' : ''} mt-10 mb-5 text-lg font-semibold p-2 px-7 rounded-lg drop-shadow-lg cursor-pointer bg-zinc-300 dark:bg-zinc-600 transition duration-300 hover:bg-zinc-400 dark:hover:bg-zinc-500 hover:-translate-y-2 hover:scale-110`} type="submit" value={
+                        success
+                            ?
+                                'Post Pubblicato'
+                            :
+                                error ? 'Errore nella pubblicazione' : 'Crea Post'
+                    } />
                 </form>
             </div>
         </div>
--- pages/api/upload.js
+++ pages/api/upload.js
@@ -5,19 +5,23 @@ export default async (req, res) => {
         console.log('fetching...');
     console.log('req: ', req.body);
     cloudinary.config({
-        cloud_name: 'dzq0qjvxg',
-        api_key: '91789788989898',
+        cloud_name: 'dcrsevgpq',
+        api_key: '611918792535995',
         api_secret: 'fuPTAZ6whNJkG6CBilru3IeoAJ0',
     })
 
+    console.log(req.path);
+        // console.log(req.path.join(__dirname, '../../../public/images/' + req.body.fileName));
+
     try {
-        const result = await cloudinary.uploader.upload(req.name, {
+        const result = await cloudinary.uploader.upload(req.path, {
             use_filename: true,
             unique_filename: false,
         })
         console.log('result: ', result);
         const data = result.json()
-        console.log('data: ', data || null);
+        console.log('data: ', data);
+        console.log('data: ', 'data || null');
         
     } catch (error) {
         console.log('upload error: ', error);
